aab3ca1c2cdef2a39323708e204c5aadb69aae1e
Rahmawati Nurul Janah <63674912+rahmawnj@users.noreply.github.com>
2026-09-18 15:31:48 +0700

    Use hardcoded API URL on app startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
-import 'api/api_service.dart';
-import 'pages/api_config_page.dart';
 import 'pages/api_login_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final baseUrl = await ApiConfig.getBaseUrl();
-  runApp(MyApp(hasApiConfig: baseUrl != null && baseUrl.isNotEmpty));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key, required this.hasApiConfig});
-
-  final bool hasApiConfig;
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +18,7 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Arial',
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xff18bdd7)),
       ),
-      home: hasApiConfig ? const ApiLoginPage() : const ApiConfigPage(),
+      home: const ApiLoginPage(),
     );
   }
 }
