d4979ddd0c24fd2416914d62e12d83b2cd362ba2
Rahmawati Nurul Janah <63674912+rahmawnj@users.noreply.github.com>
2026-09-23 09:52:29 +0700

    Fix navigation route compile error

--- lib/app_navigation.dart
+++ lib/app_navigation.dart
@@ -45,28 +45,12 @@ class AppNavigation {
     Navigator.of(context).pushReplacement(_route(pageForIndex(index, userName)));
   }
 
-  static PageRouteBuilder _route(
-    Widget page, {
-    required bool fromRight,
-  }) {
+  static PageRouteBuilder _route(Widget page) {
     return PageRouteBuilder(
       pageBuilder: (_, __, ___) => page,
-      transitionDuration: const Duration(milliseconds: 280),
-      reverseTransitionDuration: const Duration(milliseconds: 220),
-      transitionsBuilder: (_, animation, __, child) {
-        final curved = CurvedAnimation(
-          parent: animation,
-          curve: Curves.easeOutCubic,
-        );
-
-        return SlideTransition(
-          position: Tween<Offset>(
-            begin: Offset(fromRight ? 1 : -1, 0),
-            end: Offset.zero,
-          ).animate(curved),
-          child: child,
-        );
-      },
+      transitionDuration: Duration.zero,
+      reverseTransitionDuration: Duration.zero,
+      transitionsBuilder: (_, __, ___, child) => child,
     );
   }
 }
