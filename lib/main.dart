0e04f30dafb5ab8e0811dd550dded27dfdcb908a
Rahmawati Nurul Janah <63674912+rahmawnj@users.noreply.github.com>
2026-09-23 15:32:40 +0700

    Remove hardcoded product title

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Smile RS',
+      title: 'Linen Management',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'Arial',
