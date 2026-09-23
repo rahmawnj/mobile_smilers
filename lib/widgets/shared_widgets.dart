f57461b8fcaf238740fc39b6a801e177c840683e
Rahmawati Nurul Janah <63674912+rahmawnj@users.noreply.github.com>
2026-09-23 13:51:22 +0700

    Center active navbar background vertically

--- lib/widgets/shared_widgets.dart
+++ lib/widgets/shared_widgets.dart
@@ -1366,7 +1366,7 @@ class BottomNavigation extends StatelessWidget {
                             ? activePosition * itemWidth
                             : activePosition * itemWidth + qrWidth) +
                         itemWidth * .11,
-                    top: 5,
+                    top: 3,
                     width: itemWidth * .78,
                     height: 44,
                     child: IgnorePointer(
