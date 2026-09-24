8c8391fb901ec4b7b64c47fee077150fd200e360
Rahmawati Nurul Janah <63674912+rahmawnj@users.noreply.github.com>
2026-09-24 10:54:26 +0700

    Import standalone ruangan page in shared widgets

--- lib/widgets/shared_widgets.dart
+++ lib/widgets/shared_widgets.dart
@@ -11,6 +11,7 @@ import '../pages/rekap_page.dart';
 import '../pages/belum_kembali_page.dart';
 import '../pages/detail_pages.dart';
 import '../pages/ready/linen_ready_page.dart';
+import '../pages/ruangan/linen_ruangan_page.dart';
 import '../app_navigation.dart';
 
 class TableSurface extends StatelessWidget {
